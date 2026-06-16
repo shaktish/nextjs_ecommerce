3edf8bd0e98354991731d93db8427c80def9aafb
= <=>
2026-06-16 15:58:47 +0530

    middleware fix

--- client/next.config.ts
+++ client/next.config.ts
@@ -6,13 +6,10 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5010/api/:path*", // your backend
+        destination: `${process.env.API_URL}/api/:path*`, // your backend
       },
     ];
   },
-  // env: {
-  //   NEXT_PUBLIC_API_URL: process.env.API_URL,
-  // },
   images: {
     remotePatterns: [
       {
