38bb275be6109050610b868a6732f7eb69fbd1a9
= <=>
2026-06-17 15:09:10 +0530

    revalidate-fix

--- client/src/app/api/revalidate-product/route.ts
+++ client/src/app/api/revalidate-product/route.ts
@@ -1,6 +1,7 @@
 import { revalidateTag } from "next/cache";
 
 export async function POST(req: Request) {
+  console.log("revalidate fn called");
   const secret = req.headers.get("x-revalidate-secret");
 
   if (secret !== process.env.REVALIDATE_SECRET) {
@@ -11,6 +12,7 @@ export async function POST(req: Request) {
   const { productSlug } = data;
   revalidateTag("products", "max");
   revalidateTag(`product-${productSlug}`, "max");
+  console.log("validated");
   return Response.json({
     success: true,
   });
