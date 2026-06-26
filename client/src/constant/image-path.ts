126a5ab1b32650a08e2c1a35e30fcad2d6c7fc0a
= <=>
2026-06-26 13:24:11 +0530

    Fixed siteurl

--- client/src/constant/image-path.ts
+++ client/src/constant/image-path.ts
@@ -1,3 +1,3 @@
 const siteUrl = process.env.NEXT_PUBLIC_APP_URL!;
 
-export const DEFAULT_PRODUCT_IMAGE = `${siteUrl}/images/default-product.png`;
+export const DEFAULT_PRODUCT_IMAGE = `${siteUrl}images/default-product.png`;
